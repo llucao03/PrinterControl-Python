0dc02d5d000b48ae3abbb265d112e6fc97c5f8b6
Lucas <luccsscarvalho@gmail.com>
2024-04-12 22:19:26 -0300

    update.me

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from app import db, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def current_user(user_id):
+    return usuario.query.get(user_id)
+
+class cadastros(db.Model, UserMixin):
+    __tablename__ = "cadastros"
+    os = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(255), nullable=False)
+    fone = db.Column(db.String(255), nullable=False)
+    marca = db.Column(db.String(255), nullable=False)
+    modelo = db.Column(db.String(255), nullable=False)
+    nserie = db.Column(db.String(255), nullable=False)
+    defeito = db.Column(db.String(255), nullable=False)
+    status = db.Column(db.String(255), nullable=False)
+    data = db.Column(db.DateTime,  default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
